27c6403fd8b8510f7bdf8b32f08c99c4dd0f96ea
Alex Davis <acdavis050@gmail.com>
2021-07-29 11:09:03 -0700

    Add TileContent component

--- client/spotify-camera-obscura-client/src/components/Explorer/TileContent/TileContent.js
+++ client/spotify-camera-obscura-client/src/components/Explorer/TileContent/TileContent.js
@@ -1,22 +1,35 @@
+import { Tooltip } from '@material-ui/core';
+import { ToolTipContent } from './CalendarToolTip/ToolTipContent';
+
 const TileContent = function ({ date, tracks, setSelectedDateRange }) {
     function hoverTile(date) {
         const start = new Date(date);
         const end = new Date(date);
         // todo will need to create a version for months and years as well
         end.setUTCHours(23, 59, 59, 999);
-        console.log([start, end]);
         setSelectedDateRange([start, end]);
     }
 
     function exitTile() {
         setSelectedDateRange([0, 0]);
     }
+
+    function hoverToolTip() {
+        console.log('hovered');
+    }
     return (
-        <div onMouseEnter={hoverTile.bind(null, date)} onMouseLeave={exitTile}>
-            {tracks.map((trackInfo) => (
-                <p key={trackInfo.track.id}>{trackInfo.track.name}</p>
-            ))}
-        </div>
+        <Tooltip
+            interactive={true}
+            arrow={true}
+            title={<ToolTipContent tracks={tracks} />}
+        >
+            <div
+                onMouseEnter={hoverTile.bind(null, date)}
+                onMouseLeave={exitTile}
+            >
+                <i>{tracks.length ? tracks.length + ' songs' : ''}</i>
+            </div>
+        </Tooltip>
     );
 };
 
