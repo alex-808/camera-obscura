89eec04cb891438b76a7a284cf94e74b13cbe0f2
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-11-26 13:11:42 -0800

    Add JSS

--- client/spotify-camera-obscura-client/src/components/Home/Home.jsx
+++ client/spotify-camera-obscura-client/src/components/Home/Home.jsx
@@ -4,14 +4,14 @@ import { useState } from 'react';
 import { testTracks, testTracks2 } from '../../testData/testData';
 
 const Home = function (props) {
-    // const [trackData, setTrackData] = useState(0);
-    const [trackData, setTrackData] = useState(testTracks2.sorted);
+  //const [trackData, setTrackData] = useState(0);
+  const [trackData, setTrackData] = useState(testTracks2.sorted);
 
-    if (!trackData) {
-        return <PlaylistPicker setTrackData={setTrackData} />;
-    } else {
-        return <Explorer trackData={trackData} />;
-    }
+  if (!trackData) {
+    return <PlaylistPicker setTrackData={setTrackData} />;
+  } else {
+    return <Explorer trackData={trackData} />;
+  }
 };
 
 export { Home };
