c916fbcf400857d545dfcd07fa2afb17d547befc
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-12-21 15:51:55 -0800

    Add initial font and styling for graph

--- client/spotify-camera-obscura-client/src/components/Explorer/Explorer.jsx
+++ client/spotify-camera-obscura-client/src/components/Explorer/Explorer.jsx
@@ -1,128 +1,139 @@
-import { useState } from 'react';
-import { Calendar } from '../Calendar.jsx';
-import { GraphHandler } from './GraphHandler/GraphHandler';
-import { Header } from '../Header/Header.jsx';
-import * as dates from '../../utils/dates';
+import { useState } from 'react'
+import { Calendar } from '../Calendar.jsx'
+import { GraphHandler } from './GraphHandler/GraphHandler'
+import { Header } from '../Header/Header.jsx'
+import * as dates from '../../utils/dates'
 import {
   ANALYSIS_FEATURES,
   Dataset,
   CHART_TYPES,
   CHART_COLORS,
-} from '../../utils/charts';
-import { DateFeatures } from '../../utils/dateFeatures.js';
-import { averageConcurrentTracks } from './average';
+} from '../../utils/charts'
+import { DateFeatures } from '../../utils/dateFeatures.js'
+import { averageConcurrentTracks } from './average'
 
 // ! Dates and times are relative to the user's current timezone
 const Explorer = function ({ trackData }) {
-  const defaultDate = { activeStartDate: new Date(), view: 'month' };
+  const defaultDate = { activeStartDate: new Date(), view: 'month' }
 
-  const [selectedDateRange, setSelectedDateRange] = useState();
+  const [selectedDateRange, setSelectedDateRange] = useState()
   const [graphDatasets, setGraphDatasets] = useState(
     createEmptyDataSets(ANALYSIS_FEATURES)
-  );
-  const [currentView, setCurrentView] = useState(defaultDate.view);
+  )
+  const [currentView, setCurrentView] = useState(defaultDate.view)
 
   function updateDataSets(activeStartDate, view, tracks) {
     if (view !== 'single_track') {
-      const range = dates.getViewsDateRange(activeStartDate, view);
-      tracks = getTracksInRange(range, tracks);
+      const range = dates.getViewsDateRange(activeStartDate, view)
+      tracks = getTracksInRange(range, tracks)
     }
 
-    const { datasets } = createGraphData(tracks, view);
-    setCurrentView(view);
-    setGraphDatasets(datasets);
+    const { datasets } = createGraphData(tracks, view)
+    setCurrentView(view)
+    setGraphDatasets(datasets)
   }
 
   function getTracksInRange(dateRange, tracks) {
-    const tracksInRange = [];
+    const tracksInRange = []
     for (let [trackDate, trackInfo] of Object.entries(tracks)) {
-      trackDate = new Date(trackDate);
+      trackDate = new Date(trackDate)
       if (trackDate >= dateRange[0] && trackDate < dateRange[1]) {
-        tracksInRange.push(trackInfo);
+        tracksInRange.push(trackInfo)
       }
     }
 
-    return tracksInRange;
+    return tracksInRange
   }
 
   function createGraphData(tracks, view) {
-    if (!tracks) return { datasets: null };
+    if (!tracks) return { datasets: null }
 
-    let dateFeatures = convertTracksToDateFeatures(tracks);
+    let dateFeatures = convertTracksToDateFeatures(tracks)
 
     if (view !== 'day' && view !== 'single_track') {
-      const viewMethods = new dates.ViewMethods(view);
-      dateFeatures = averageConcurrentTracks(dateFeatures, viewMethods || []);
+      const viewMethods = new dates.ViewMethods(view)
+      dateFeatures = averageConcurrentTracks(dateFeatures, viewMethods || [])
     }
 
-    const datasets = createGraphTypeDatasets(dateFeatures);
+    const datasets = createGraphTypeDatasets(dateFeatures)
 
-    return { datasets: datasets };
+    return { datasets: datasets }
   }
 
   function createGraphTypeDatasets(dateFeatures) {
-    const graphType = getGraphType(dateFeatures);
-    const datasets = createEmptyDataSets(ANALYSIS_FEATURES);
+    const graphType = getGraphType(dateFeatures)
+    const datasets = createEmptyDataSets(ANALYSIS_FEATURES)
     for (let dateFeature of dateFeatures) {
-      let trackDate = new Date(dateFeature.date);
+      let trackDate = new Date(dateFeature.date)
       for (let dataset of datasets) {
         if (graphType === CHART_TYPES.Linegraph) {
           dataset.data.push({
             date: trackDate,
             value: dateFeature.analysisFeatures[`${dataset.label}`],
-          });
+          })
         } else {
           dataset.data.push({
             label: dataset.label,
             value: dateFeature.analysisFeatures[`${dataset.label}`],
-          });
+          })
         }
       }
     }
-    return datasets;
+    return datasets
   }
 
   const getGraphType = function (dateFeatures) {
-    if (dateFeatures.length !== 1) return CHART_TYPES.Linegraph;
-    else return CHART_TYPES.Bargraph;
-  };
+    if (dateFeatures.length !== 1) return CHART_TYPES.Linegraph
+    else return CHART_TYPES.Bargraph
+  }
 
   function convertTracksToDateFeatures(tracks) {
     return tracks.map(
-      (trackInfo) =>
+      trackInfo =>
         new DateFeatures(trackInfo.added_at, trackInfo.analysis_features)
-    );
+    )
   }
 
   function createEmptyDataSets(labels) {
-    const datasets = [];
-    let i = 0;
+    const datasets = []
+    let i = 0
     for (let label of labels) {
-      datasets.push(new Dataset(label, [], CHART_COLORS[i], CHART_COLORS[i]));
-      i++;
+      datasets.push(new Dataset(label, [], CHART_COLORS[i], CHART_COLORS[i]))
+      i++
     }
-    return datasets;
+    return datasets
   }
 
   if (graphDatasets) {
     return (
       <>
         <Header />
-        <GraphHandler
-          datasets={graphDatasets}
-          currentView={currentView}
-          selectedDateRange={selectedDateRange}
-        />
-        <Calendar
-          trackData={trackData}
-          setSelectedDateRange={setSelectedDateRange}
-          updateDataSets={updateDataSets}
-        />
+        <div style={{ width: '80%', margin: 'auto' }}>
+          <GraphHandler
+            datasets={graphDatasets}
+            currentView={currentView}
+            selectedDateRange={selectedDateRange}
+          />
+        </div>
+        <div
+          style={{
+            width: '80%',
+            margin: 'auto',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Calendar
+            trackData={trackData}
+            setSelectedDateRange={setSelectedDateRange}
+            updateDataSets={updateDataSets}
+          />
+        </div>
       </>
-    );
+    )
   } else {
-    return <>Loading</>;
+    return <>Loading</>
   }
-};
+}
 
-export { Explorer };
+export { Explorer }
--- client/spotify-camera-obscura-client/src/components/Explorer/GraphHandler/Linegraph/Linegraph.jsx
+++ client/spotify-camera-obscura-client/src/components/Explorer/GraphHandler/Linegraph/Linegraph.jsx
@@ -1,148 +1,169 @@
-import { useEffect, useRef } from 'react';
-import * as dateUtils from '../../../../utils/dates';
-import { Line } from 'react-chartjs-2';
-import 'chartjs-adapter-luxon';
+import { useEffect, useRef } from 'react'
+import * as dateUtils from '../../../../utils/dates'
+import { Line } from 'react-chartjs-2'
+import 'chartjs-adapter-luxon'
 
 const Linegraph = function ({
-    datasets,
-    view,
-    selectedDateRange = [0, 0],
-    enabledFeatures,
-    onLegendClick,
-    onLegendHover,
+  datasets,
+  view,
+  selectedDateRange = [0, 0],
+  enabledFeatures,
+  onLegendClick,
+  onLegendHover,
 }) {
-    const chartRef = useRef();
-    const data = {
-        datasets: datasets,
-    };
+  const chartRef = useRef()
+  const data = {
+    datasets: datasets,
+  }
 
-    const viewMethods = new dateUtils.ViewMethods(view);
+  const viewMethods = new dateUtils.ViewMethods(view)
 
-    useEffect(() => {
-        const chart = chartRef.current;
-        updateSelectedElements(chart);
-    });
+  useEffect(() => {
+    const chart = chartRef.current
+    updateSelectedElements(chart)
+  })
 
-    function updateSelectedElements(chart) {
-        if (!chart) return;
-        let data = chart.data.datasets;
-        const isSameTimePeriod = viewMethods.getComparer();
+  function updateSelectedElements(chart) {
+    if (!chart) return
+    let data = chart.data.datasets
+    const isSameTimePeriod = viewMethods.getComparer()
 
-        const selectedIndex = data[0].data.findIndex((el) => {
-            return isSameTimePeriod(el.date, selectedDateRange[0]);
-        });
+    const selectedIndex = data[0].data.findIndex(el => {
+      return isSameTimePeriod(el.date, selectedDateRange[0])
+    })
 
-        if (selectedDateRange[0] === 0 || selectedIndex === -1) {
-            disableAllElements(chart);
-        } else {
-            activateSelectedElements(chart, selectedIndex);
-        }
-        chart.render();
+    if (selectedDateRange[0] === 0 || selectedIndex === -1) {
+      disableAllElements(chart)
+    } else {
+      activateSelectedElements(chart, selectedIndex)
+    }
+    chart.render()
+  }
+
+  function activateSelectedElements(chart, selectedIndex) {
+    const activeElements = []
+    const n = Object.entries(datasets).length
+    for (let i = 0; i < n; i++) {
+      if (enabledFeatures.includes(datasets[i].label)) {
+        activeElements.push({
+          datasetIndex: i,
+          index: selectedIndex,
+        })
+      }
+    }
+    if (!activeElements.length) return
+    chart.setActiveElements(activeElements)
+    chart.tooltip.setActiveElements(activeElements)
+  }
+
+  function disableAllElements(chart) {
+    chart.setActiveElements([])
+    chart.tooltip.setActiveElements([])
+  }
+
+  const setupOpts = function (view, opts) {
+    const viewTooltipFormats = {
+      day: {
+        tooltipFormat: 'DD ttt',
+        tickUnit: 'hour',
+      },
+      month: {
+        tooltipFormat: 'DD',
+        tickUnit: 'day',
+      },
+      year: { tooltipFormat: 'MMM yyyy', tickUnit: 'month' },
+      decade: { tooltipFormat: 'yyyy', tickUnit: 'year' },
     }
 
-    function activateSelectedElements(chart, selectedIndex) {
-        const activeElements = [];
-        const n = Object.entries(datasets).length;
-        for (let i = 0; i < n; i++) {
-            if (enabledFeatures.includes(datasets[i].label)) {
-                activeElements.push({
-                    datasetIndex: i,
-                    index: selectedIndex,
-                });
-            }
-        }
-        if (!activeElements.length) return;
-        chart.setActiveElements(activeElements);
-        chart.tooltip.setActiveElements(activeElements);
-    }
+    opts.scales.xAxes.time.tooltipFormat =
+      viewTooltipFormats[`${view}`].tooltipFormat
+    opts.scales.xAxes.time.unit = viewTooltipFormats[`${view}`].tickUnit
 
-    function disableAllElements(chart) {
-        chart.setActiveElements([]);
-        chart.tooltip.setActiveElements([]);
-    }
+    return opts
+  }
 
-    const setupOpts = function (view, opts) {
-        const viewTooltipFormats = {
-            day: {
-                tooltipFormat: 'DD ttt',
-                tickUnit: 'hour',
+  let options = {
+    animation: false,
+    parsing: {
+      xAxisKey: 'date',
+      yAxisKey: 'value',
+    },
+    responsive: true,
+    interaction: {
+      intersect: false,
+      mode: 'index',
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            font: {
+              family: 'Arial',
+              size: 14,
             },
-            month: {
-                tooltipFormat: 'DD',
-                tickUnit: 'day',
-            },
-            year: { tooltipFormat: 'MMM yyyy', tickUnit: 'month' },
-            decade: { tooltipFormat: 'yyyy', tickUnit: 'year' },
-        };
-
-        opts.scales.xAxes.time.tooltipFormat =
-            viewTooltipFormats[`${view}`].tooltipFormat;
-        opts.scales.xAxes.time.unit = viewTooltipFormats[`${view}`].tickUnit;
-
-        return opts;
-    };
-
-    let options = {
-        animation: false,
-        parsing: {
-            xAxisKey: 'date',
-            yAxisKey: 'value',
+          },
         },
-        responsive: true,
-        interaction: {
-            intersect: false,
-            mode: 'index',
+      ],
+      xAxes: {
+        type: 'time',
+        time: {
+          tooltipFormat: 'MMMM yy',
+          unit: 'month',
         },
-        scales: {
-            yAxes: [
-                {
-                    ticks: {
-                        beginAtZero: true,
-                    },
-                },
-            ],
-            xAxes: {
-                type: 'time',
-                time: {
-                    tooltipFormat: 'MMMM yy',
-                    unit: 'month',
-                },
-            },
+        ticks: {
+          beginAtZero: true,
+          font: {
+            family: 'Arial',
+            size: 14,
+          },
         },
-        plugins: {
-            legend: {
-                onHover: onLegendHover,
-                onClick: onLegendClick,
-            },
+      },
+    },
+    plugins: {
+      legend: {
+        onHover: onLegendHover,
+        onClick: onLegendClick,
+        position: 'bottom',
+        labels: {
+          font: {
+            family: 'Roboto',
+            size: 16,
+          },
+          boxWidth: 40,
+          boxHeight: 40,
+          usePointStyle: true,
         },
+      },
+    },
 
-        spanGaps: true,
-        elements: {
-            point: {
-                borderWidth: 15,
-            },
-            line: {
-                borderWidth: 15,
-            },
-        },
-    };
+    spanGaps: true,
+    elements: {
+      point: {
+        borderWidth: 5,
+      },
+      line: {
+        borderWidth: 2,
+      },
+    },
+  }
 
-    options = setupOpts(view, options);
+  options = setupOpts(view, options)
 
-    const onClick = function (props) {
-        console.log(props);
-    };
+  const onClick = function (props) {
+    console.log(props)
+  }
 
-    return (
-        <>
-            <Line
-                ref={chartRef}
-                data={data}
-                options={options}
-                getElementAtEvent={onClick}
-            />
-        </>
-    );
-};
+  return (
+    <>
+      <Line
+        ref={chartRef}
+        data={data}
+        options={options}
+        getElementAtEvent={onClick}
+      />
+    </>
+  )
+}
 
-export { Linegraph };
+export { Linegraph }
